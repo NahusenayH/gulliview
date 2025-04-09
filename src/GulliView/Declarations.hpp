a146563950638b881bf205309a47cbff14e52db1
AliShirzad <alishir@chalmers.se>
2025-04-09 14:34:37 +0200

    Added licenseing to all files

--- src/GulliView/Declarations.hpp
+++ src/GulliView/Declarations.hpp
@@ -1,3 +1,20 @@
+/*********************************************************************
+* This file is distributed as part of the C++ port of the APRIL tags
+* library. The code is licensed under GPLv2.
+*
+* Original author: Edwin Olson <ebolson@umich.edu>
+* C++ port and modifications: Matt Zucker <mzucker1@swarthmore.edu>
+* ----------------------- Modified ---------------------------------e
+* Code modified for project in Vision Based Localization for
+* Autonomous Vehicles at Chalmers University, Goteborg, Sweden
+* Modification Authors:
+* Copyright (c) 2013-2014 Andrew Soderberg-Rivkin <sandrew@student.chalmers.se>
+* Copyright (c) 2013-2014 Sanjana Hangal <sanjana@student.chalmers.se>
+* Copyright (c) 2014 Thomas Petig <petig@chalmers.se>
+* Copyright (c) 2025 Emil Nylander <emilnyla@chalmers.se>
+* Copyright (c) 2025 Elias Svensson <eliasve@chalmers.se>
+********************************************************************/
+
 #ifndef _DECLARATIONS_H_
 #define _DECLARATIONS_H_
 
