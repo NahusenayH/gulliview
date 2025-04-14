61f03ed02f0a5625f6dcae1a1f2f838ca6c47533
AliShirzad <alishir@chalmers.se>
2025-04-14 09:18:16 +0200

    Merged

--- src/GulliView/Declarations.hpp
+++ src/GulliView/Declarations.hpp
@@ -28,9 +28,9 @@
 // This is for visualize_GulliView_logs
 // Version string, adds to time period ex VT25.2
 #define TIME_PERIOD "VT25"
-#define VERSION "37"
+#define VERSION "38"
 // change this text to denote version, this is saved by log script to catagorize
-#define COMMENT "Logging in fast thread done"
+#define COMMENT "Merged with main"
 
 #define ENABLE_LOGS             true            // Enables log output files
 #define LIVE_FEED               false           // If the cameras live feed or recordings from RECORDING_FOLDER are used
